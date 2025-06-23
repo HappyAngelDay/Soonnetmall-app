8ef4c1f2df365870ed11bb1a236ed2faff5e225c
Alice Lai <happy.angel.dayday@gmail.com>
2025-06-23 14:43:24 +0800

    Add vite vue plugin and set base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/soonnetmall-app/' // GitHub Pages 子路徑，根據你 repo 名改
 })
