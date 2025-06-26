fb8b2dbcf8cd6659bca3864ffa391c02493c1092
Alice Lai <happy.angel.dayday@gmail.com>
2025-06-26 14:13:25 +0800

    Auto commit before deploy

---
+++ src/data/records.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    id: 1,
+    title: '2025 長榮馬拉松',
+    count: 128, // ✅ 上傳張數
+    cover: 'https://source.unsplash.com/random/300x300?run1' // ✅ 封面圖
+  },
+  {
+    id: 2,
+    title: '大嶼山馬拉松 2025',
+    count: 96,
+    cover: 'https://source.unsplash.com/random/300x300?run2'
+  }
+]
